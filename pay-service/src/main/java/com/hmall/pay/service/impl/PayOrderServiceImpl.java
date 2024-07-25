8390b5597dbdb444abee03ab7fd98a86bcdc4ec8
Justin <191302290@qq.com>
2024-07-25 11:40:32 +0800

    对更新订单状态进行业务幂等处理

--- pay-service/src/main/java/com/hmall/pay/service/impl/PayOrderServiceImpl.java
+++ pay-service/src/main/java/com/hmall/pay/service/impl/PayOrderServiceImpl.java
@@ -16,6 +16,9 @@ import com.hmall.pay.enums.PayStatus;
 import com.hmall.pay.mapper.PayOrderMapper;
 import com.hmall.pay.service.IPayOrderService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,6 +33,7 @@ import java.time.LocalDateTime;
  * @author 虎哥
  * @since 2023-05-16
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class PayOrderServiceImpl extends ServiceImpl<PayOrderMapper, PayOrder> implements IPayOrderService {
@@ -38,6 +42,8 @@ public class PayOrderServiceImpl extends ServiceImpl<PayOrderMapper, PayOrder> i
   private final UserClient userClient;
 
   private final TradeClient tradeClient;
+
+  private final RabbitTemplate rabbitTemplate;
 
     @Override
     public String applyPayOrder(PayApplyDTO applyDTO) {
@@ -64,8 +70,13 @@ public class PayOrderServiceImpl extends ServiceImpl<PayOrderMapper, PayOrder> i
         if (!success) {
             throw new BizIllegalException("交易已支付或关闭！");
         }
-        // 5.修改订单状态
-        tradeClient.markOrderPaySuccess(po.getBizOrderNo());
+        //TODO 5.修改订单状态
+        //tradeClient.markOrderPaySuccess(po.getBizOrderNo());
+        try {
+            rabbitTemplate.convertAndSend("pay.direct","pay.success",po.getPayOrderNo());
+        } catch (AmqpException e) {
+            log.error("发送支付成功消息失败,订单id:{}",po.getPayOrderNo(),e);
+        }
     }
 
     public boolean markPayOrderSuccess(Long id, LocalDateTime successTime) {
---
+++ trade-service/src/main/java/com/hmall/trade/listener/PayStatusListener.java
@@ -0,0 +1,37 @@
+package com.hmall.trade.listener;
+
+import com.hmall.trade.domain.po.Order;
+import com.hmall.trade.service.IOrderService;
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PayStatusListener {
+
+    private final IOrderService orderService;
+
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue(value = "trade.pay.success.queue",durable = "true"),
+            exchange = @Exchange(value = "pay.direct"),
+            key = "pay.success"
+    ))
+    public void listenPaySuccess(Long orderId) {
+        //1.查询订单
+        Order order = orderService.getById(orderId);
+
+        //2.判断订单状态，是否为未支付
+        if(order==null||order.getStatus()!=1){
+            //不做处理
+            return;
+        }
+
+        //3.标记订单状态为已支付
+        orderService.markOrderPaySuccess(orderId);
+    }
+}
